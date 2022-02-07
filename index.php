035debf571c91caed0637ae297364b347aacaa9c
entrilas <53130677+entrilas@users.noreply.github.com>
2022-02-07 16:58:49 +0200

    namespaces update

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ spl_autoload_register(function($class){
      require_once((dirname(__FILE__).'/'.str_replace('\\', '/',$class).'.php'));
 });
 
-$fileReader = new \App\Services\FileReaderService();
-$logger = new \App\Console\Logger();
-$content = $fileReader->readFilesTest(\App\Constants\Constants::US_TEX_PATTERNS);
+$fileReader = new App\Services\FileReaderService();
+$content = $fileReader->readFilesTest(App\Constants\Constants::US_TEX_PATTERNS);
 $algorithm = new App\Algorithm\Hyphenation($content);
 
-Logger::time("Hyphenation Algorithm");
+App\Console\Logger::time("Hyphenation Algorithm");
 $hyphenatedWord = $algorithm->hyphenate("mistranslate");
-Logger::info($hyphenatedWord);
-Logger::timeEnd("Hyphenation Algorithm");
+App\Console\Logger::info($hyphenatedWord);
+App\Console\Logger::timeEnd("Hyphenation Algorithm");
